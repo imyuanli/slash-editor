f707ce45bf6c93a934546911a4f670e20b7ed490
yuanli <2865437316@qq.com>
2023-09-05 17:46:04 +0800

    feat: 增加有序无序列表

--- package/slash-editor.tsx
+++ package/slash-editor.tsx
@@ -2,23 +2,50 @@
 
 import {EditorContent, useEditor} from '@tiptap/react';
 import {defaultExtensions} from "@/package/extensions";
+import {forwardRef, useImperativeHandle} from "react";
 
-const SlashEditor: any = () => {
+interface SlashEditorProps {
+    className?: string;
+    extensions?: [];
+    ref?: any;
+}
+
+const SlashEditor: React.FunctionComponent<SlashEditorProps> = forwardRef((
+    {
+        className = 'relative min-h-[480px] w-full max-w-screen-lg border-stone-200 sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:shadow-lg',
+        extensions,
+    },
+    ref
+) => {
+
     const editor = useEditor({
-        extensions: defaultExtensions,
+        extensions: [...defaultExtensions, extensions],
         editorProps: {
             attributes: {
-                class: 'prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none h-full w-full',
+                class: 'prose prose-stone dark:prose-invert prose-sm sm:prose-base lg:prose-lg m-5 focus:outline-none h-full w-full',
             },
         },
+        autofocus: 'end',
         content: ``,
     });
 
+    useImperativeHandle(
+        ref,
+        () => ({
+            editor,
+        })
+    );
+
+    const getFocus = () => editor?.chain().focus().run();
+
     return (
-        <div className={'relative min-h-[480px] w-full max-w-screen-lg border-stone-200 bg-white sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:shadow-lg'}>
-            <EditorContent className={'h-full'} editor={editor}/>
+        <div
+            onClick={getFocus}
+            className={className}
+        >
+            <EditorContent editor={editor}/>
         </div>
     );
-}
+})
 
 export default SlashEditor
--- package/suggestions.tsx
+++ package/suggestions.tsx
@@ -1,7 +1,7 @@
 import {
     Heading1,
     Heading2,
-    Heading3,
+    Heading3, List, ListOrdered,
     Pilcrow,
     Quote,
 } from "lucide-react";
@@ -11,56 +11,67 @@ interface CommandProps {
     range: any;
 }
 
-const getHeadingCommand = (level: number) => {
-    return ({editor, range}: CommandProps) => {
-        editor
-            .chain()
-            .focus()
-            .deleteRange(range)
-            .setNode("heading", {level})
-            .run();
-    };
-}
+const getCommand = (props: CommandProps) => props?.editor.chain().focus().deleteRange(props?.range)
+
+const getHeadingCommand = (level: number) => (props: CommandProps) => getCommand(props).setNode("heading", {level}).run();
 
 const Suggestions = ({query}: { query: string }) => [
     {
         title: "Paragraph",
-        description: "Paragraph",
+        description: "Just start typing with plain text.",
         icon: <Pilcrow/>,
-        command: getHeadingCommand(1),
-    },
-    {
-        title: "Heading 1",
-        description: "Heading 1",
-        icon: <Heading1/>,
-        command: getHeadingCommand(1),
-    },
-    {
-        title: "Heading 2",
-        description: "Heading 1",
-        icon: <Heading2/>,
-        command: getHeadingCommand(2),
-    },
-    {
-        title: "Heading 3",
-        description: "Heading 1",
-        icon: <Heading3/>,
-        command: getHeadingCommand(3),
-    },
-    {
-        title: "Blockquote",
-        description: "Heading 1",
-        icon: <Quote/>,
         command: ({editor, range}: CommandProps) => {
             editor
                 .chain()
                 .focus()
                 .deleteRange(range)
                 .toggleNode("paragraph", "paragraph")
-                .toggleBlockquote("blockquote")
+                .run();
+        }
+    },
+    {
+        title: "Heading 1",
+        description: "Big section heading.",
+        icon: <Heading1/>,
+        command: getHeadingCommand(1),
+    },
+    {
+        title: "Heading 2",
+        description: "Medium section heading.",
+        icon: <Heading2/>,
+        command: getHeadingCommand(2),
+    },
+    {
+        title: "Heading 3",
+        description: "Small section heading.",
+        icon: <Heading3/>,
+        command: getHeadingCommand(3),
+    },
+    {
+        title: "Blockquote",
+        description: "Capture a quote.",
+        icon: <Quote/>,
+        command: ({editor, range}: CommandProps) => {
+            editor
+                .chain()
+                .focus()
+                .deleteRange(range)
+                .toggleBlockquote()
                 .run();
         },
-    }
+    },
+    {
+        title: 'Bullet List',
+        description: 'Create a simple bullet list.',
+        icon: <List />,
+        command: (props: CommandProps) => getCommand(props).toggleBulletList().run()
+    },
+    {
+        title: 'Ordered List',
+        description: 'Create a list with numbering.',
+        icon: <ListOrdered />,
+        command: (props: CommandProps) => getCommand(props).toggleOrderedList().run()
+    },
 ]
 
 export default Suggestions
