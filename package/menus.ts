08527e840905d662c6de12ac938f23f3a0d3fda3
yuanli <2865437316@qq.com>
2023-09-07 12:01:23 +0800

    fix: 修复标题无效的bug

--- package/menus.ts
+++ package/menus.ts
@@ -4,7 +4,7 @@ import {
     Heading1,
     Heading2,
     Heading3,
-    Heading4,
+    Heading4, Heading5,
     Italic, List,
     Pilcrow, Quote,
     Strikethrough, TerminalSquare,
@@ -24,7 +24,7 @@ const handleCommand = ({editor, range}: any) => {
     }
 }
 
-const handleHeadingCommand = (level: number) => (props: CommandProps) => handleCommand(props).toggleNode("heading", {level}).run()
+const handleHeadingCommand = (props: CommandProps, level: number) => handleCommand(props).toggleHeading({level}).run()
 
 const isActive = (editor: any, type: string) => editor.isActive(type)
 
@@ -43,37 +43,37 @@ export const textMenu = [
         title: "Heading 1",
         description: "Big section heading.",
         icon: Heading1,
-        command: handleHeadingCommand(1),
+        command: (props: CommandProps) => handleHeadingCommand(props, 1),
         isActive: (editor: any) => isHeadingActive(editor, 1)
     },
     {
         title: "Heading 2",
         description: "Medium section heading.",
         icon: Heading2,
-        command: handleHeadingCommand(2),
+        command: (props: CommandProps) => handleHeadingCommand(props, 2),
         isActive: (editor: any) => isHeadingActive(editor, 2)
     },
     {
         title: "Heading 3",
         description: "Small section heading.",
         icon: Heading3,
-        command: handleHeadingCommand(3),
+        command: (props: CommandProps) => handleHeadingCommand(props, 3),
         isActive: (editor: any) => isHeadingActive(editor, 3)
     },
     {
         title: "Heading 4",
         description: "Smaller section heading.",
         icon: Heading4,
-        command: handleHeadingCommand(4),
+        command: (props: CommandProps) => handleHeadingCommand(props, 4),
         isActive: (editor: any) => isHeadingActive(editor, 4)
     },
     {
         title: "Heading 5",
         description: "Smallest section heading.",
-        icon: Heading4,
-        command: handleHeadingCommand(5),
+        icon: Heading5,
+        command: (props: CommandProps) => handleHeadingCommand(props, 5),
         isActive: (editor: any) => isHeadingActive(editor, 5)
-    },
+    }
 ]
 
 //标记相关的菜单
