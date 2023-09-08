d38fdfade45423ae1eadf02080a4f961f2d25066
yuanli <2865437316@qq.com>
2023-09-08 10:19:01 +0800

    refactor: 更换标题

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import packageJson from '@/package.json'
 import Nav from "@/components/nav";
 import SlashEditor from "@/package/slash-editor";
 
@@ -6,7 +5,6 @@ export default function Home() {
     return (
         <main className={'relative min-h-screen h-full w-full flex flex-col items-center p-4'}>
             <Nav/>
-            <div className={'text-3xl font-bold font-mono my-16'}>Slash Editor {packageJson?.version}</div>
             <SlashEditor/>
         </main>
     )
--- package/default-content.ts
+++ package/default-content.ts
@@ -1,6 +1,8 @@
+import packageJson from '@/package.json'
+
 const defaultContent = `
     <div>
-      <h1>Using SlashEditor introduce @tailwindcss/typography</h1>
+      <h1>Slash Editor ${packageJson?.version}</h1>
       <p class="lead">Until now, trying to style an article, document, or blog post with Tailwind has been a tedious task that required a keen eye for typography and a lot of complex custom CSS.</p>
       <p>By default, Tailwind removes all of the default browser styling from paragraphs, headings, lists and more. This ends up being really useful for building application UIs because you spend less time undoing user-agent styles, but when you <em>really are</em> just trying to style some content that came from a rich-text editor in a CMS or a markdown file, it can be surprising and unintuitive.</p>
       <p>We get lots of complaints about it actually, with people regularly asking us things like:</p>
