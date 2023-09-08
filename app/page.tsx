54125971c669f5735e839bca4babccc7062fee13
yuanli <2865437316@qq.com>
2023-09-08 10:18:37 +0800

    refactor: 去除多余组件

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import Editor from "@/components/editor";
 import packageJson from '@/package.json'
 import Nav from "@/components/nav";
+import SlashEditor from "@/package/slash-editor";
 
 export default function Home() {
     return (
         <main className={'relative min-h-screen h-full w-full flex flex-col items-center p-4'}>
             <Nav/>
             <div className={'text-3xl font-bold font-mono my-16'}>Slash Editor {packageJson?.version}</div>
-            <Editor/>
+            <SlashEditor/>
         </main>
     )
 }
